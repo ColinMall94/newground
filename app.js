9695b63bc385339003ba1ca5d0e81cd0513c361a
ColinMall94 <58265780+ColinMall94@users.noreply.github.com>
2019-11-28 15:24:37 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-	
-var ipString = myFunction.toString()
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
